dc6f4340abde00768a16522f5395d64b29b94b47
Kapibarra <nikita.nov2016@yandex.ru>
2022-11-23 13:24:46 +0300

    fix init

---
+++ ngRepairCourse/src/app/homepage/shared/components/first-lesson/first-lesson.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-lesson',
+  templateUrl: './first-lesson.component.html',
+  styleUrls: ['./first-lesson.component.scss']
+})
+export class FirstLessonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
