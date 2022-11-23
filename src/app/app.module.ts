0a0d407fb79e3fa29af9a300816005b9cc880b68
Kapibarra <nikita.nov2016@yandex.ru>
2022-11-23 13:14:05 +0300

    init sructure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainpageComponent } from './homepage/mainpage/mainpage.component';
+import { HeroComponent } from './homepage/shared/components/hero/hero.component';
+import { AboutComponent } from './homepage/shared/components/about/about.component';
+import { AdvantagesComponent } from './homepage/shared/components/advantages/advantages.component';
+import { PriceComponent } from './homepage/shared/components/price/price.component';
+import { FirstLessonComponent } from './homepage/shared/components/first-lesson/first-lesson.component';
+import { GalleryComponent } from './homepage/shared/components/gallery/gallery.component';
+import { MarketComponent } from './homepage/shared/components/market/market.component';
+import { ContactsComponent } from './homepage/shared/components/contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainpageComponent,
+    HeroComponent,
+    AboutComponent,
+    AdvantagesComponent,
+    PriceComponent,
+    FirstLessonComponent,
+    GalleryComponent,
+    MarketComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
