15d60c972b7d517856c1f744c08b173a9f016219
Kapibarra <nikita.nov2016@yandex.ru>
2022-11-24 13:23:09 +0300

    advantages

--- ngRepairCourse/src/app/homepage/shared/components/advantages/advantages.component.ts
+++ ngRepairCourse/src/app/homepage/shared/components/advantages/advantages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Advantages } from '../../interface/advantages';
 
 @Component({
   selector: 'app-advantages',
@@ -6,7 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./advantages.component.scss']
 })
 export class AdvantagesComponent implements OnInit {
-
+advantages:Advantages[] = [ {
+  id: '01',
+  title: 'ВЫСОКАЯ ЗАРПЛАТА',
+  description: 'Увеличьте свой заработок, начав работать в сфере ремонта и обслуживания электронной техники: это всегда востребованная и хорошо оплачиваемая сфера.',
+  fillBg: false,
+},
+{
+  id: '02',
+  title: 'ОПЫТНЫЕ НАСТАВНИКИ',
+  description: 'С реальным опытом работы в сервисных центрах, ремонте и обслуживании электронной техники, научат  ВСЕМ тонкостям своего дела',
+  fillBg: false,
+},
+{
+  id: '03',
+  title: 'ЛУЧШЕЕ ОБОРУДОВАНИЕ',
+  description: 'Полностью оборудованные классы позволят максимально погрузиться в практические занятия под присмотром НАСТОЯЩИХ профи',
+  fillBg: false,
+},
+{
+  id: '04',
+  title: 'ВОСТРЕБОВАННОСТЬ',
+  description: 'специалист решает разноплановые задачи: перепрошивает смартфоны, снимает блокировку, меняет комплектующие (разбитые экраны или вышедшие из строя аккумуляторы).',
+  fillBg: true,
+},
+]
   constructor() { }
 
   ngOnInit(): void {
---
+++ ngRepairCourse/src/app/homepage/shared/interface/advantages.ts
@@ -0,0 +1,12 @@
+export class Advantages {
+    id: string;
+    title: string;
+    description: string;
+    fillBg: boolean;
+    constructor(advantages:Advantages) {
+this.id = advantages.id;
+this.title = advantages.title;
+this.description = advantages.description;
+this.fillBg = advantages.fillBg;
+    }
+}
