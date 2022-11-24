feda2edf64c7621b2c1fe448322c0cf4e16c464e
Kapibarra <nikita.nov2016@yandex.ru>
2022-11-24 13:54:04 +0300

    header

--- ngRepairCourse/src/app/homepage/shared/components/header/header.component.ts
+++ ngRepairCourse/src/app/homepage/shared/components/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Constants } from './../../constants';
+import { ViewportScroller } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  isSticky: boolean = false;
+  mobile: boolean = false;
+  isFilled: boolean = true;
+  phoneNumber: string = Constants.phone;
+  constructor(private viewportscroller: ViewportScroller) { }
 
   ngOnInit(): void {
+    if (window.screen.width <= 968) {
+      this.mobile = true;
+      console.log(this.mobile);
+    }
   }
-
+  onClickScroll(elId: string): void {
+    this.viewportscroller.scrollToAnchor(elId);
+  }
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  onScroll(event: any) {
+    const verticalOffset = window.pageYOffset;
+    verticalOffset > 40 ? (this.isSticky = true) : (this.isSticky = false);
+  }
 }
---
+++ ngRepairCourse/src/app/homepage/shared/constants.ts
@@ -0,0 +1,4 @@
+export class Constants {
+    public static phone: string = '+7 495 777 10 68';
+  }
+  
